899909750d70f913ac7750dcf7bbdf10cd0b58fb
mpolaczyk <marcin.polaczyk@gmail.com>
2015-01-23 21:30:04 +0100

    Sprite research done.

--- research/spite/main.lua
+++ research/spite/main.lua
@@ -1,10 +1,24 @@
 
+
 function love.load()
    cat = love.graphics.newImage("cat.jpg")
-   x = 50
-   y = 50
+  
+   
+end
+
+function love.update(dt)
+	
 end
 
 function love.draw()
-   love.graphics.draw(cat, x, y)
+	draw(cat,50, 200, 90)
+	draw(cat,50, 50, -90)
+	draw(cat,200, 50, 135)
+	draw(cat,200, 200, 180)
+end
+
+function draw(image, _x, _y, _rot)
+	local width = image:getWidth()
+	local height = image:getHeight()
+	love.graphics.draw(image, _x, _y, math.rad(_rot), 1, 1, width/2, height/2)
 end
