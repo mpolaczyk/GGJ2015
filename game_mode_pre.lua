53502bc4d966f913221fbdcb04c857b2bd3410cb
mpolaczyk <marcin.polaczyk@gmail.com>
2015-01-24 12:24:32 +0100

    Pre screen waiting for assets.

--- game_mode_pre.lua
+++ game_mode_pre.lua
@@ -12,7 +12,11 @@ function gameModePreClass.new(gameState)
 end
 
 function gameModePreClass:load()
-    
+    --self.eventLogo = love.graphics.newImage("img/wsad.png")
+	--self.titleLogo = love.graphics.newImage("img/wsad.png")
+	--self.teamImage = love.graphics.newImage("img/wsad.png")
+	--self.rulesImage = love.graphics.newImage("img/wsad.png")
+	--self.enterImage = love.graphics.newImage("img/wsad.png")
 end
 
 function gameModePreClass:update(dt)
@@ -20,8 +24,23 @@ function gameModePreClass:update(dt)
 end
 
 function gameModePreClass:draw()
-    
-    love.graphics.print("This is " .. self.moduleName .. " module", 10, 10)
+    -- draw event logo
+	--love.graphics.draw(self.eventImage, x, y, 0, 1, 1, 0, 0)
+	
+	-- draw title logo
+	--love.graphics.draw(self.titleImage, x, y, 0, 1, 1, 0, 0)
+	
+	-- draw team  texts
+	--love.graphics.draw(self.teamImage, x, y, 0, 1, 1, 0, 0)
+	
+	-- draw rules image
+	--love.graphics.draw(self.teamImage, x, y, 0, 1, 1, 0, 0)
+	
+	-- draw enter button
+	--love.graphics.draw(self.enterImage, x, y, 0, 1, 1, 0, 0)
+	
+	-- temp until assets arrive
+    love.graphics.print("Hit enter to continue!", 10, 10)
 end
 
 function gameModePreClass:keypressed(key)
